952e07337c4b4ba2cae576eddd86c231cf4cb88d
0xshoegazer <0xshoegazer@gmail.com>
2024-03-15 20:00:18 -0400

    testnet deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@ import { deployFactory } from './utils/contract.utils'
 
 async function main() {
   const signer = (await ethers.getSigners())[0]
-  await deployFactory('0xFDa619b6d20975be80A10332cD39b9a4b0FAa8BB', signer)
+  await deployFactory('0xB687282AD4Fb8897D5Cd41f3C1A54DeB4cc88625', signer)
 }
 
 main().catch((error) => {
